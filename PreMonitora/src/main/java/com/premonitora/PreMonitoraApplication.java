884045bd31b207a6b19787f2408f7970cd96092c
henri-nog <fellipe.caluete@a.ficr.edu.br>
2025-06-07 10:24:59 -0300

    Projeto PreMonitora com Spring Boot

---
+++ PreMonitora/src/main/java/com/premonitora/PreMonitoraApplication.java
@@ -0,0 +1,12 @@
+package com.premonitora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreMonitoraApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PreMonitoraApplication.class, args);
+	}
+
+}
